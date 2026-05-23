d0e74cebf8601979e266344c77502fc2918651aa
Vitsarut Udomphol <udomphol2@gmail.com>
2026-05-23 23:51:52 +0700

    make medream-studio.com to be the primary site

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,16 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.medream-studio.com' }],
+        destination: 'https://medream-studio.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default withNextIntl(nextConfig)
